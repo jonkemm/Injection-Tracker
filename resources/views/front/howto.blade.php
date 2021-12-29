348137cb86ba5e0b53dc2a24bdc37850e971c417
wheeliemanuk <wheeliemanuk@gmail.com>
2021-12-29 20:57:51 +0000

    tarty bits

--- resources/views/front/howto.blade.php
+++ resources/views/front/howto.blade.php
@@ -9,14 +9,22 @@ How to, instructions
 @endsection 
 <div class="container">
     <x-guest-layout>
+        <div class="card">  
+            <div class="card-body">
+                <h5 class="card-title">Join up</h5>
+                <p class="card-text">
+                    <a href="{{ route('register') }}">register</a> or <a href="{{ route('login') }}">login</a>, why not be extra secure and log in with Google?
+                </p>
+                <a href="#" class="btn btn-success">Login</a> &nbsp; s<a href="#" class="btn btn-primary">Register</a>
+            </div>
+        </div>
         <div class="card">
-            <ol>
-                <li><a href="{{ route('register') }}">register</a> or <a href="{{ route('login') }}">login</a></li>
-                <li>In the <a href="{{ route('locations') }}">locations</a> section, name the location of your first image (e.g. Right Leg) and upload the image.</li>
-                <li>Choose the location you created and inject yourself</li>
-                <li>Double click on the image in the place where you injected</li>
-                <li>Rate the pain and add the site</li>
-            </ol>
+                    <img src="/img/thigh-example-front-2.png" class="card-img-left" style="width:50%" alt="List of injection sites" >
+                    In the <a href="{{ route('locations') }}">locations</a> section, name the location of your first image (e.g. Right Leg) and upload the image.
+        </div>
+        <div class="card">
+                    <img src="/img/thigh-example-front.png" style="width:50%" alt="List of injection sites" >
+                    Choose the location you created and inject yourself, double click on the image in the place where you injected, rate the pain and add the site
         </div>
     </x-app-layout>
 </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,52 +1,36 @@
-
-        <nav class="navbar navbar-expand-lg">
-            <div class="container">
-                <a href="/">Injection Tracker</a>
-                <div class="ml-auto my-2 my-lg-0">
-                    <div>
-                        @auth
-                            <a href="{{ route('profile') }}">
-                            @if( Auth::user()->avatar )
-                            <img src="{{ Auth::user()->avatar }}" class="avatar" alt="Avatar" />
-                            @else
-                            {{Auth::user()->name}}
-                            @endif
-                             </a>
-                        @else
-                            <a href="{{ route('login') }}">Log in</a>
-
-                            @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                            @endif
-                        @endauth
-                    </div>
-
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand ml-2" href="/">Injection Tracker</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="{{ route('features') }}">Features</a>
+                    <a class="nav-item nav-link" href="{{ route('howto') }}">How to</a>
                 </div>
+                @auth
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-item nav-link" href="{{ route('locations') }}">Locations</a>
+                </div>
+                @endauth
+            </div>
+            <div class="ml-auto my-2 my-lg-0">
+                @auth
+                    <a href="{{ route('profile') }}">
+                    @if( Auth::user()->avatar )
+                    <img src="{{ Auth::user()->avatar }}" class="avatar" alt="Avatar" />
+                    @else
+                    {{Auth::user()->name}}
+                    @endif
+                        </a>
+                @else
+                    <a href="{{ route('login') }}">Log in</a>
+
+                    @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+
             </div>
         </nav>
-
-
-        <nav class="navbar sub-nav navbar-expand-lg">
-            <div class="container">
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">           
-                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a href="{{ route('home') }}">Introduction</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('howto') }}">How to use</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('features') }}">Features</a>
-                        </li>
-                    </ul>
-                    @auth
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a href="{{ route('locations') }}">Locations</a>
-                        </li>
-                    </ul>   
-                    @endauth     
-                </div>
-            </div>
-        </nav>
+        <div class="subnav bg-gradient">j</div>
